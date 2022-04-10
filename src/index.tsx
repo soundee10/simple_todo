97e0ad28e818b5d2e48c986e4c3f0c93e291e444
unknown <malice@korea.ac.kr>
2022-04-10 22:37:06 +0900

    #1.1 To Do setup, without library, basic way

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
 import App from './App';
+import { darkTheme } from './theme';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
