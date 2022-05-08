e126a42fec941591c3efb84d522c4bad3f5d21b7
unknown <malice@korea.ac.kr>
2022-05-09 02:19:18 +0900

    #2.4 Multi board dragging todos

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,9 @@ function App() {
     const onDragEnd = (info:DropResult) => {
         console.log(info);
         const { destination, draggableId, source} = info;
+        if (!destination) return;
+        
+        // same board drag
         if (destination?.droppableId === source.droppableId){
             setToDos((allBoards) =>{
             const boardCopy = [...allBoards[source.droppableId]];
@@ -39,6 +42,25 @@ function App() {
             };
             });
         }
+        // inter board drag
+        if (destination.droppableId !== source.droppableId){
+            setToDos((allBoards)=>{
+                const sourceBoard = [...allBoards[source.droppableId]];
+                const destinationBoard = [...allBoards[destination.droppableId]];
+                sourceBoard.splice(source.index, 1);
+                destinationBoard.splice(destination.index, 0, draggableId);
+            
+            return{
+                ...allBoards,
+                [source.droppableId]: sourceBoard,
+                [destination.droppableId]: destinationBoard,
+            };
+            }
+            );
+        }
+
+
+
     };
     return (
     <DragDropContext onDragEnd={onDragEnd}>
